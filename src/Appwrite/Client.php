e916a4ad9361cccb0e8d92f8e3c7b93d661bbaac
Eldad Fux <eldad.fux@gmail.com>
2021-05-19 08:52:36 +0300

    Test 0.10.1 fix

--- src/Appwrite/Client.php
+++ src/Appwrite/Client.php
@@ -35,7 +35,7 @@ class Client
      */
     protected $headers = [
         'content-type' => '',
-        'x-sdk-version' => 'appwrite:php:2.0.0',
+        'x-sdk-version' => 'appwrite:php:2.1.0',
     ];
 
     /**
